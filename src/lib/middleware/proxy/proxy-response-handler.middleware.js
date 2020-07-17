bae94ddd05d76fdf8eaf78af1530d2200bc1a154
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2020-07-17 17:28:45 -0400

    ADDED get_transactions USE CASE TO PROXY RESPONSE HANDLER.

--- src/lib/middleware/proxy/proxy-response-handler.middleware.js
+++ src/lib/middleware/proxy/proxy-response-handler.middleware.js
@@ -1,4 +1,13 @@
 /**
+ * The methods below applying formatting to records returned from the data service.
+ * This logic is located here because the structure of the `express-http-proxy` package does
+ * not allow for easy post-processing of responses from proxied API requests. The sole 
+ * facility for achieving post-processing is through the user-configured `userResDecorator` 
+ * method (see proxy-resolve.middleware)
+ */
+
+
+/**
 * _ALL_ methods return a specified entity according to the JSON Schema specfication in 
 * the /schemas folder.
 * @param {Object} proxyRes - proxied service's Express response object
@@ -43,6 +52,13 @@ module.exports = {
             data: [
                 ...proxyResData
             ]
+        }
+    },
+    //Transactions
+    "get:/api/transactions": function (proxyRes, proxyResData, userReq, userRes) {
+        return {
+            entries: proxyResData.length,
+            data: proxyResData
         }
     }
 
